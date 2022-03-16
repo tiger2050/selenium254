93fc364ddfb0f9535c979b94988bffb32662bf78
farhad <fzkhanfred@gmail.com>
2022-03-15 22:14:28 -0300

    1st commit on tuesday

---
+++ src/test/java/jenkinspackage/JenkinsPgm.java
@@ -0,0 +1,22 @@
+package jenkinspackage;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPgm {
+
+	@Test
+	public void hello()    	{
+		System.out.println("hello jenkins");
+	}
+	
+	@Test
+	public void methodtwo()    	{
+		System.out.println("good morning");
+	}
+	
+	@Test
+	public void jenkinsthree()    	{
+		System.out.println("hi");
+	}
+	
+}
---
+++ src/test/java/jenkinspackage/JenkinsPrgm2.java
@@ -0,0 +1,23 @@
+package jenkinspackage;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPrgm2 {
+
+	@Test
+	public void hello()    	{
+		System.out.println("hello jenkins");
+	}
+	
+	@Test
+	public void methodtwo()    	{
+		System.out.println("good morning");
+	}
+	
+	@Test
+	public void jenkinsthree()    	{
+		System.out.println("hi");
+	}
+	
+	
+}
